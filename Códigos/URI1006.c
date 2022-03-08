1291cb2316918fc13bf2b35ef683b3352fe21caa
Camila <camilagabriel45@gmail.com>
2022-03-08 20:57:25 -0300

    primeiro

--- Códigos/URI1006.c
+++ Códigos/URI1006.c
@@ -2,7 +2,18 @@
  
 int main() {
  
-    printf("Hello World!\n");
- 
+    float A, B, C, notaA, notaB, notaC, media; 
+
+    scanf ("%f %f %f", &A, &B, &C);
+
+    notaA = (A * 2)/10;
+    notaB = (B * 3)/10;
+    notaC = (C * 5)/10;
+
+    media = notaA + notaB + notaC;
+
+    printf ("MEDIA = %.1f\n", media);
+
+
     return 0;
 }
